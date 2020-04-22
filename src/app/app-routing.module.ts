73807c78295b099e86d2b437136e57b8b47be391
sanuvish11 <46244188+sanuvish11@users.noreply.github.com>
2020-04-22 13:59:09 +0530

    this angular 8 9 10 base project amaozn web services use
    this angular 8 9 10 base project amaozn web services use

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,62 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuardService } from './auth-guard.service';
+import {ProfileComponent} from'./profile/profile.component';
+import {SettingsComponent} from'./settings/settings.component';
+import {AddressComponent} from'./address/address.component';
+import {CategoriesComponent} from'./categories/categories.component';
+import {PostProductComponent}from'./post-product/post-product.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'categories',
+    component: CategoriesComponent,
+  },
+  
+  {
+    path: 'profile/postproduct',
+    component: PostProductComponent,
+    canActivate: [AuthGuardService],
+  },
+  {
+    path: 'register',
+    component: RegistrationComponent,
+    canActivate: [AuthGuardService],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [AuthGuardService],
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuardService],
+  },
+  {
+    path: 'profile/settings',
+    component: SettingsComponent,
+    canActivate: [AuthGuardService],
+  },
+  {
+    path: 'profile/address',
+    component: AddressComponent,
+    canActivate: [AuthGuardService],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
